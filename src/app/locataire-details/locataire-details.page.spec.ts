a06dd18c5413a67c63b27075bff34b2f7e679f28
jberger <julien.berger1421@gmail.com>
2023-12-17 14:31:52 +0100

    Getting locataire details - structure

--- src/app/locataire-details/locataire-details.page.spec.ts
+++ src/app/locataire-details/locataire-details.page.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
 import { LocataireDetailsPage } from './locataire-details.page';
 
 describe('LocataireDetailsPage', () => {
   let component: LocataireDetailsPage;
   let fixture: ComponentFixture<LocataireDetailsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LocataireDetailsPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LocataireDetailsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
