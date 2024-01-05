0bd3cb04d54c28cf58cd2c2ba5b8103af466c4ae
jurocknsail <julien.berger1421@gmail.com>
2024-01-05 17:42:09 +0100

    pdf export

--- src/app/tab-send/tab-send.page.ts
+++ src/app/tab-send/tab-send.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../storage-service.service';
 import { Leaseholder } from '../leaseholder';
+import * as pdfMake from 'pdfmake/build/pdfmake';  
+import * as pdfFonts from 'pdfmake/build/vfs_fonts'; 
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
 
 @Component({
   selector: 'app-tab-send',
@@ -12,12 +16,36 @@ export class TabSendPage implements OnInit {
 
   constructor(
     private storageService: StorageService
-  ) {}
+  ) {
+  }
 
-  ngOnInit() {
+  async ngOnInit() {
 
-    // Find the product that correspond with the id provided in route.
+    await this.storageService.loadLeaseholders();
     this.leaseholders = this.storageService.getLeaseholders();
+
+    if (this.leaseholders == undefined) {
+      this.storageService.getJson().subscribe(data => {
+        this.leaseholders = data;
+        this.storageService.set("data", this.leaseholders);
+        this.storageService.setLeaseholders(this.leaseholders);
+        console.log("No local data, assets data loaded and stored : " + JSON.stringify(this.leaseholders));
+      });
+    } else {
+      console.log("Local data loaded : " + JSON.stringify(this.leaseholders));
+    }
    
   }
+
+  generatePdf() {
+    const docDefinition: TDocumentDefinitions = {
+       content: [
+         'First paragraph',
+         'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines',
+       ],
+    };
+    pdfMake.createPdf(docDefinition).open();
+
+  }
+
 }
