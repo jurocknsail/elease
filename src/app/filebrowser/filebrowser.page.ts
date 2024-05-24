0a0c83356df7396c5a4050e817ca4f0b7152d868
jurocknsail <julien.berger1421@gmail.com>
2024-05-24 14:23:53 +0200

    update

--- src/app/filebrowser/filebrowser.page.ts
+++ src/app/filebrowser/filebrowser.page.ts
@@ -118,7 +118,6 @@ export class FilebrowserPage implements OnInit {
   sendEmail() {
     this.parseService.getLeaseholdersPDFs().forEach( (pdfDataList, email) => {
       pdfDataList.forEach((pdfData) => {
-        console.log("AAA " + email)
         this.parseService.sendEmail(email, pdfData).subscribe({
           next: (res) => {
             console.log('Email sent successfully !');
